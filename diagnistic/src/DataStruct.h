81b189ee10d7edfd62239419fbfb3515136e3442
slayerrensky <rensky.g@googlemail.com>
2014-10-02 00:20:51 +0200

    diagnostic hinzugefügt und fehler in EnOcean.cpp berichtig

---
+++ diagnistic/src/DataStruct.h
@@ -0,0 +1,20 @@
+/*
+ * dataStruct.h
+ *
+ *  Created on: 04.08.2014
+ *      Author: rensky
+ */
+
+#ifndef DATASTRUCT_H_
+#define DATASTRUCT_H_
+
+
+struct valuePack {
+	double valuesAsSumm;
+	int numberOfValues;
+	int impulsConst;
+	long lastTs;
+};
+
+
+#endif /* DATASTRUCT_H_ */
