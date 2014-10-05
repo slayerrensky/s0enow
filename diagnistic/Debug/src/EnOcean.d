c1163a284777789b962a4afc86a09a0558350cd2
slayerrensky <rensky.g@googlemail.com>
2014-10-06 00:32:20 +0200

    commit old

--- diagnistic/Debug/src/EnOcean.d
+++
@@ -1,5 +0,0 @@
-src/EnOcean.d: ../src/EnOcean.cpp ../src/EnOcean.h ../src/DataStruct.h
-
-../src/EnOcean.h:
-
-../src/DataStruct.h:
--- diagnistic/Debug/src/enocidia.d
+++
@@ -1,5 +0,0 @@
-src/enocidia.d: ../src/enocidia.cpp ../src/EnOcean.h ../src/DataStruct.h
-
-../src/EnOcean.h:
-
-../src/DataStruct.h:
